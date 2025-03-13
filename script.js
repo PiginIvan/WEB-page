6fe4a2c9aa9c0277c5af38a90ba6f98012ebad4d
PiginIvan <piginivan4@gmail.com>
2025-03-13 21:21:23 +0300

    code refactoring

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// Переключение темы
+const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
+let currentIndex = 0;
+
+// Функция для раскрытия и скрытия текста
+function toggleText(id, btn) {
+    let textBlock = document.getElementById(id);
+    if (textBlock.style.display === "none" || textBlock.style.display === "") {
+        textBlock.style.display = "inline";
+        btn.textContent = "Скрыть";
+    } 
+    else {
+        textBlock.style.display = "none";
+        btn.textContent = "...";
+    }
+}
+
+// EventListener на переключение темы
 document.getElementById("themeButton").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
@@ -12,28 +28,14 @@ document.querySelectorAll("nav a").forEach(anchor => {
     });
 });
 
-// Слайдер изображений
-const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
-let currentIndex = 0;
-
+// EventListener на переключение на следующее изображение
 document.getElementById("nextBtn").addEventListener("click", function() {
     currentIndex = (currentIndex + 1) % images.length;
     document.getElementById("sliderImage").src = "images/" + images[currentIndex];
 });
 
+// EventListener на переключение на предыдущее изображение
 document.getElementById("prevBtn").addEventListener("click", function() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     document.getElementById("sliderImage").src = "images/" + images[currentIndex];
 });
-
-// Функция для раскрытия скрытого текста
-function toggleText(id, btn) {
-    let textBlock = document.getElementById(id);
-    if (textBlock.style.display === "none" || textBlock.style.display === "") {
-        textBlock.style.display = "inline";
-        btn.textContent = "Скрыть";
-    } else {
-        textBlock.style.display = "none";
-        btn.textContent = "...";
-    }
-}
